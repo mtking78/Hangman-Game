0db1dc40301598209b6c6a6c8db4a63f7154a0b4
mtking78 <mtking78@gmail.com>
2018-02-07 12:26:14 -0700

    userGuesses pushes to array and displays on screen.  next step is to check word for userGuess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var gameStart = true;
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 10;
-var words = ["a", "bb", "ccc", "dddd", "eeeee"];
+var words = ["armenia", "belarus", "chad", "denmark", "eritrea"];
 var letters = ["a", "b", "c", "d", "e",
                 "f", "g", "h", "i", "j",
                 "k", "l", "m", "n", "o",
@@ -16,10 +16,11 @@ var lettersLeft = ["a", "b", "c", "d", "e",
                 "k", "l", "m", "n", "o",
                 "p", "q", "r", "s", "t",
                 "u", "v", "w", "x", "y", "z"];
+// delete letters left[], use indexof to check lettersGuessed with -1
 var wordSplit = "";
 var currentWord = "";
 var answerArray = [];
-var lettersGuessed = [];
+var userGuesses = [];
 
 
 // ***** FUNCTIONS ***** //
@@ -31,33 +32,36 @@ document.onclick = function(event) {
         document.getElementById("wins").innerHTML = wins;
         document.getElementById("losses").innerHTML = losses;
         document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+        document.getElementById("lettersGuessed").innerHTML = userGuesses;
 
 
 // Randomly select a word from the "words" array
-    var currentWord = words[Math.floor(Math.random() * words.length)];
+    currentWord = words[Math.floor(Math.random() * words.length)];
     console.log(currentWord);
+
 
 // Fill the game screen with blanks that match the length of the word
     for (var i = 0; i < currentWord.length; i++) {
         answerArray[i] = "_";
     }
-    console.log(event.key);
     console.log(answerArray);
+
+// Populates blanks in the "Current Word" span
+    document.getElementById("currentWord").innerHTML = answerArray.join(" ");
 
 // Splits the current word into individual letters
     wordSplit = currentWord.split("");
     lettersLeft = wordSplit.length;
     console.log(wordSplit);
 
-// Populates blanks in the "Current Word" span
-// Must be a space in-between quotes else one whole line
-    document.getElementById("currentWord").innerHTML = answerArray.join(" ");
-
 // Populates letters guessed in the "Letters Guessed" span
+    document.onkeyup = function (event) {
+        console.log(event.key);
+        userGuesses.push(event.key);
+        document.getElementById("lettersGuessed").innerHTML = userGuesses;
+    }
 
-// This prevents key strokes from resetting the current word to solve.
-// Without this, the user cannot type keys to guess letters.
+// This prevents key strokes from resetting the current word to solve.  Without this, the user cannot type keys to guess letters.
     gameStart = false;
     }
 }
@@ -70,31 +74,27 @@ document.onclick = function(event) {
 //        console.log("no");
 //    }
 //}
-//function letterChecker(x) {
-//        var letter = x.charAt();
-//        if (letter === "") {
-//            console.log("yes");
-//        } else {
-//            console.log("no");
-//        }
-//}
+//document.addEventListener('keyup', function(letterChecker) {
 
-function lettersGuessed () {
-    var lettersGuessed = document.getElementById("lettersGuessed").value;
-    if (lettersGuessed.length > 0) {
-        for (var i = 0; i < currentWord.length; i++) {
-            if (currentWord[i] === lettersGuessed) {
-                answerArray[i] = lettersGuessed;
-            }
-        }
-    }
-    guessesRemaining--;
-    document.getElementById("guessesRemaining").innerHTML = "No of clicks: " + count;
-    document.getElementById("currentWord").innerHTML = answerArray.join(" ");
-}
+    //    for (var j = 0; j <currentWord.length; int++) {
+//     console.log(currentWord[j].indexOf('a') >= 0);
+//    }
+//})
+
+//function lettersGuessed () {
+//    var lettersGuessed = document.getElementById("lettersGuessed").value;
+//    if (lettersGuessed.length > 0) {
+//        for (var i = 0; i < currentWord.length; i++) {
+//            if (currentWord[i] === lettersGuessed) {
+//                answerArray[i] = lettersGuessed;
+//            }
+//        }
+//    }
+//    guessesRemaining--;
+//    document.getElementById("guessesRemaining").innerHTML = "No of clicks: " + count;
+//    document.getElementById("currentWord").innerHTML = answerArray.join(" ");
+//}
 
 // ***** MAIN PROCESS ***** //
 // -------------------------------------------------------------------------------------------------------
 
-document.onkeyup = function (event) {
-}
